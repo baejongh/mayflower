b0b6506cc7c03fcef3a56ca8963ff906a6d08157
Jong Hoon Bae <baejongh@grinnell.edu>
2017-03-01 20:23:15 +0000

    login page is working

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_pages/userProfile'
+  #get 'static_pages/userProfile'
+
+  #devise_for :users
 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'settings' }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'application#hello'
+  root 'application#hello'
 
   get '/home', to: 'application#home', as: 'home'
   get '/secret', to: 'application#secret', as: 'secret'
