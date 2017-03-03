3623dea402bd027cc5e9dd7f108ea6fb92629ed6
Jong Hoon Bae <baejongh@grinnell.edu>
2017-03-03 20:15:10 +0000

    committing for testing branch so we can exit testing branch

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'settings' }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root :to => redirect("/login")
 
   get '/home', to: 'application#home', as: 'home'
   get '/secret', to: 'application#secret', as: 'secret'
